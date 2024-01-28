1d2787ce3055d4bd1520aa72e0a26d3e40328598
BrianP8701 <brianp8701@gmail.com>
2024-01-28 10:11:49 -0500

    added alot of shit to the action space

--- aga_swarm/actions/reasoning/decompose_directive/decompose_directive.py
+++ aga_swarm/actions/reasoning/decompose_directive/decompose_directive.py
@@ -5,16 +5,16 @@ import traceback
 from aga_swarm.swarm.types import NodeOutput, SwarmID, SwarmCommand, LifecycleCommand, NodeOutput
 from aga_swarm.swarm.oai_agent import OAI_Agent
 
-def manager(directive: str, swarm_id: SwarmID) -> NodeOutput:
-    with open('aga_swarm/actions/reasoning/manager/tool.json', 'r') as file:
-        manager_blueprint = json.load(file)
+def decompose_directive(directive: str, swarm_id: SwarmID) -> NodeOutput:
+    with open('aga_swarm/actions/reasoning/decompose_directive/tool.json', 'r') as file:
+        decompose_directive_blueprint = json.load(file)
     
-    tools = manager_blueprint['manager']['tools']
-    instructions = manager_blueprint['manager']['instructions']
-    manager = OAI_Agent(instructions=instructions, tools=tools, tool_choice="break_down_directive", openai_key=swarm_id.configs.openai_key)
+    tools = decompose_directive_blueprint['decompose_directive']['tools']
+    instructions = decompose_directive_blueprint['decompose_directive']['instructions']
+    decompose_directive = OAI_Agent(instructions=instructions, tools=tools, tool_choice="break_down_directive", openai_key=swarm_id.configs.openai_key)
     
     try:
-        subdirectives = manager.chat(directive)['subdirectives']
+        subdirectives = decompose_directive.chat(directive)['subdirectives']
     except Exception as e:
         return NodeOutput(
             lifecycle_command=LifecycleCommand.NODE_FAILURE,
@@ -41,4 +41,4 @@ def manager(directive: str, swarm_id: SwarmID) -> NodeOutput:
 
 @validate_arguments
 def main(directive: str, swarm_id: SwarmID) -> NodeOutput:
-    return manager(directive, swarm_id)
+    return decompose_directive(directive, swarm_id)
